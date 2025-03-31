8b18f035c216abdd751dae09789f914b4804ab90
EmilyLi2021 <lixiaomeng2021@gmail.com>
2025-03-30 20:43:19 -0400

    changesign_message method

--- gen_keys.py
+++ gen_keys.py
@@ -25,7 +25,7 @@ def sign_message(challenge, filename="secret_key.txt"):
     private_key = key[0].strip()
     account = eth_account.Account.from_key(private_key)
     eth_addr = account.address
-    signed_message = account.sign_message(message, private_key=private_key)
+    signed_message = account.sign_message(message)
 
     assert eth_account.Account.recover_message(message,signature=signed_message.signature.hex()) == eth_addr, f"Failed to sign message properly"
 
